e306915bb31f97af251d5a55cfdfd89056d74c77
Akshay <akshayparadkar29@gmail.com>
2023-08-26 14:34:48 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'myapp';
+
+  constructor(private fb:FormBuilder){}
+  
+  validate: FormGroup = new FormGroup({});
+  
+  ngOnInit(): void {
+    this.validate = this.fb.group({
+      fname : new FormControl('',[Validators.required,Validators.minLength(5),Validators.maxLength(10)]),
+      lname : new FormControl('',[Validators.required,Validators.minLength(5),Validators.maxLength(10)]),
+      checkbox : new FormControl('',[Validators.required])
+    });
+  }
+  
+  validateForm(){
+    console.warn(this.validate.value);
+  }
 }
